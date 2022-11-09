34bf65052add77860a1f1e41fd19676de7864527
shristi-rks <shristi.n08@gmail.com>
2022-11-09 16:08:38 +0200

    correct total

--- part-2/course-information/src/Components/Total.jsx
+++ part-2/course-information/src/Components/Total.jsx
@@ -1,5 +1,5 @@
 const Total = ({course}) => {
-    const total =course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises + course.parts[1].exercises + course.parts[3].exercises;
+    const total =course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises + course.parts[3].exercises;
     console.log(total)
     return (
         <p><strong>total of {total} excercises</strong></p>
